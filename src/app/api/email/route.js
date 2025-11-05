83a0fb1e01b2dcfe63c92b42afd58f69fc25d071
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-11-05 15:30:32 +0400

    sdasdas asdas

--- src/app/api/email/route.js
+++ src/app/api/email/route.js
@@ -84,6 +84,6 @@ export async function POST(req) {
     );
   } catch (error) {
     console.log(error);
-    return NextResponse.json({ message: error.message }, { status: 400 });
+    return NextResponse.json({ message: error.message }, { status: 400 });  
   }
 }
